ae3ebc78ca471d899ed490cebacfb7a83ad8e40e
akananthu11 <akananthu11@gmail.com>
2025-07-06 21:46:32 +0530

    feat: k s k sheet

--- src/components/Appointment/AppointmentTable.jsx
+++ src/components/Appointment/AppointmentTable.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import DataTable from "../Tables/DataTable";
-import { pdficon } from "../imagepath";
+import { pdficon, printericon } from "../imagepath";
 import DateSearchHero from "../heros/DateSearchHero";
 import { formatTime } from "../configs/formatTime";
 import CenteredModal from "../modals/CenteredModal";
 import { useState } from "react";
 import Prescription from "./Prescription";
+import { generateCaseSheetPDF } from "../../utils/pdfGenerator";
 
 function AppointmentTable(props) {
   const { appointmentList, loading, handleDate, handleSearch } = props;
@@ -24,6 +25,8 @@ function AppointmentTable(props) {
     // Open the modal to show the prescription
     setShow(true);
   };
+
+
   const columns = [
     {
       title: "Booking ID",
@@ -34,7 +37,7 @@ function AppointmentTable(props) {
       title: "Patient Name",
       dataIndex: "patientname",
       // sorter: (a, b) => a.patientname.length - b.patientname.length,
-      render: (item, record) => <div>{record?.user_details?.name ??  record?.user?.first_name}</div>,
+      render: (_item, record) => <div>{record?.user_details?.name ??  record?.user?.first_name}</div>,
     },
     {
       title: "Type",
@@ -45,7 +48,7 @@ function AppointmentTable(props) {
       title: "Time",
       dataIndex: "timeSlot",
       // sorter: (a, b) => a.time.length - b.time.length,
-      render: (item, record) => <div>{formatTime(record?.timeSlot) ?? "unassigned"}</div>,
+      render: (_item, record) => <div>{formatTime(record?.timeSlot) ?? "unassigned"}</div>,
     },
     {
       title: "Status",
@@ -68,18 +71,18 @@ function AppointmentTable(props) {
       title: "Department",
       dataIndex: "department",
       // sorter: (a, b) => a.department.length - b.department.length,
-      render: (item, record) => <div>{record?.department}</div>,
+      render: (_item, record) => <div>{record?.department}</div>,
     },
     {
       title: "Assigned",
       dataIndex: "assingned",
       // sorter: (a, b) => a.assingned.length - b.assingned.length,
-      render: (item, record) => record?.doctor?.name ? <div>Dr.{record?.doctor?.name}</div> : <div>unassigned</div>,
+      render: (_item, record) => record?.doctor?.name ? <div>Dr.{record?.doctor?.name}</div> : <div>unassigned</div>,
     },
     {
       title: "Prescription",
-      dataIndex: "action",
-      render: (item, record) => {
+      dataIndex: "prescription",
+      render: (_item, record) => {
         return (
           record?.prescriptionUrl ?
           <div style={{ display: "flex", gap: 8, paddingLeft: "20px" }}>
@@ -90,6 +93,26 @@ function AppointmentTable(props) {
           </div>
           :
           <div style={{ paddingLeft: "25px" }}>N/A</div>
+        );
+      },
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      render: (_item, record) => {
+        return (
+          <div style={{ display: "flex", gap: 8, paddingLeft: "20px" }}>
+            <Link
+              to="#"
+              onClick={(e) => {
+                e.preventDefault();
+                generateCaseSheetPDF(record);
+              }}
+              title="Print Case Sheet"
+            >
+              <img src={printericon} alt="Print Icon" width={17} />
+            </Link>
+          </div>
         );
       },
     },
